517bc5ba872cedd6aece1bcc33d4d32d5694dca6
Brady Robles <31491624+bradyrobles@users.noreply.github.com>
2021-08-03 14:46:51 -0400

    vscode broke

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Parameter Configurations
+
+    // Cached References
+    int x1;
+    int x2;
+    int x3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
