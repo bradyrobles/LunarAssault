2a878cdaaf891a07a565dd8fe100491c929c3479
Brady Robles <31491624+bradyrobles@users.noreply.github.com>
2021-08-04 11:02:45 -0400

    Added movement in local for player ship

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,10 @@ public class PlayerController : MonoBehaviour
     // Parameter Configurations
     [SerializeField] InputAction movement;
     [SerializeField] InputAction fire;
+    [SerializeField] float velocityTuner = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +31,32 @@ public class PlayerController : MonoBehaviour
         fire.Disable();
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        Debug.Log(horizontalThrow);
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-        Debug.Log(verticalThrow);
+        float xThrow = movement.ReadValue<Vector2>().x;
+        float yThrow = movement.ReadValue<Vector2>().y;
+
+
+        float rawXPos = transform.localPosition.x +
+                        CalculateOffset(xThrow);
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y +
+                        CalculateOffset(yThrow);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(
+            clampedXPos,
+            clampedYPos,
+            transform.localPosition.z
+        );
     }
+    private float CalculateOffset(float temp)
+    {
+        float offset = temp * Time.deltaTime * velocityTuner;
+        return offset;
+    }
+
 }
