ec4b4d07e0786dae23d8412b00d58581cec912dd
Brady Robles <31491624+bradyrobles@users.noreply.github.com>
2021-08-04 09:28:44 -0400

    ready for player input for movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,32 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Parameter Configurations
-
-    // Cached References
-    int x1;
-    int x2;
-    int x3;
+    [SerializeField] InputAction movement;
+    [SerializeField] InputAction fire;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    private void OnEnable()
+    {
+        movement.Enable();
+        fire.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+        fire.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        Debug.Log(horizontalThrow);
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        Debug.Log(verticalThrow);
     }
 }
